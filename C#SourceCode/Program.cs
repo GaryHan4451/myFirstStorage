feb17e07dd9a8a641d9312429113a56f79f442e4
Gary <seu_ht@126.com>
2018-10-19 15:57:04 +0800

    第一次将C#文件提交测试

---
+++ C#SourceCode/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace C_SourceCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("this is really a sexy girl");
+        }
+    }
+}
